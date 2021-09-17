0d6c5775680f294d7ad98e635e7f559c9a266fb2
ZuberDunge <iamZuberDunge@gmail.com>
2021-09-17 17:04:56 +0530

    update fonts

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+const wakeUpImg = document.querySelector('#wake-up');
+const lunchImg = document.querySelector('#lunch');
+const lunchImgbyClass = document.querySelector('.lunch');
+const nightImg = document.querySelector('#night');
+const imgChange = document.querySelector('.box-2-image');
+const imgChange2 = document.querySelector('.wake-change');
+
+
+
+imgChange.addEventListener('click', ()=>{
+    lunchImg.style.display = `none`;
+    wakeUpImg.style.display = `block`;  
+});
+
+wakeUpImg.addEventListener('click', ()=>{
+imgChange2.style.display = `none`;
+nightImg.style.display = `block`;
+});
+
+
+
+
+
+
+
+
 // Clock section start
 const hourTime = document.querySelector('.time-hour');
 const minTime = document.querySelector('.time-min');
@@ -11,7 +37,8 @@ function time() {
   var d = new Date();
   var s = d.getSeconds();
   var m = d.getMinutes();
-  var h = d.getHours();   
+  var h = d.getHours();
+   
      hourTime.innerHTML =    `${concatZero((h % 12) || 12)}`
      minTime.innerHTML  =    `${concatZero(m)}`
      secTime.innerHTML  =    `${concatZero(s)}`
